4287d27ef49223fe6d748f610cbbb9dd11c5e1a1
Kiorn Pal <kironpal1531@gmail.com>
2026-07-28 21:28:48 +0600

    created element by js and append all the data to the element and dislayed it

--- script/index.js
+++ script/index.js
@@ -1,8 +1,20 @@
 const loadlesson = () =>{
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
-    .then(json => console.log(json));
+    .then(json => displaylessons(json.data));
 }
 
+const displaylessons = (lessons) => {
+    const lessoncontainer = document.getElementById("lesson-container");
 
+    for(let lesson of lessons){
+
+        const btndiv =document.createElement("div");
+        btndiv.innerHTML = `
+        <button class="">
+        <i class="fa-solid fa-book-bookmark"></i>Lesson</button>`;
+
+        lessoncontainer.append(btndiv);
+    }
+}
 loadlesson()
